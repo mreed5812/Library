116dbb7226c73a05caf6c0918e29cd4fe75dbfd6
mreed5812 <mreed5812@gmail.com>
2019-05-11 09:50:48 -0500

    added function to update table when book is added

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 
 const addNewBook = document.querySelector("#addNewBook");
 const closeModal = document.querySelector("#closeModal");
-const addBook = document.querySelector("#addBook");
+const addBook = document.querySelector(".addBook");
 
 let myLibrary = [{name: "Outsider", author: "Steven King", pages: "700", read:false},
 		{name: "Code Complete", author: "McConnel", pages: "1000", read:false}];
 
- var html = "<table border='1|1'>";
+function loadTable(){
+
+	var html = "<table border='1|1'>";
     for (var i = 0; i < myLibrary.length; i++) {
         html+="<tr>";
         	html+="<td>"+(i+1)+"</td>";
@@ -16,25 +18,31 @@ let myLibrary = [{name: "Outsider", author: "Steven King", pages: "700", read:fa
         	html+="<td>"+myLibrary[i].read+"</td>";
         	html+="<td>"+'<i class="fa fa-trash" aria-hidden="true">'+'</i>'+"</td>";
         html+="</tr>";
-
     }
     html+="</table>";
-document.getElementById("box").innerHTML = html;
+	document.getElementById("box").innerHTML = html;
+}
 
 function Book(title, author, pages, Read){
-	this.title = title;
+	this.name = title;
 	this.author = author;
 	this.pages = pages;
-	this.read = read;
+	this.read = Read;
 }
 
 function addBookToLibrary(){
-	//do stuff here
-	alert("it worked");
+	var bookTitle = document.getElementById("inputBookTitle").value;
+	var bookAuthor = document.getElementById("inputAuthor").value;
+	var bookPages = document.getElementById("inputPages").value;
+	var bookRead = document.getElementById("readCheck").value;
+	let newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+	myLibrary.push(newBook);	
+	loadTable();
+    document.getElementById("bookForm").reset();
 }
 
 addBook.addEventListener('click', addBookToLibrary);
 
-closeModal.addEventListener('click', function(){
-	document.getElementById("bookForm").reset();
-});
+window.onload = function(){
+	loadTable();
+}
